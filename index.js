a5378025817118b8af2069f7b826ff4ab50b5854
David <davemills093@gmail.com>
2021-09-03 11:24:19 -0400

    commit tree e635994e3c42f3002d81c5ae22882dd2472581f9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(valueBlock) {
+    if (valueBlock > 42) {
+        return valueBlock - 42;
+    } else {
+        return 42 - valueBlock;
+    }
+};
+
+function distanceFromHqInFeet(valueBlock) {
+    return distanceFromHqInBlocks(valueBlock) * 264;
+};
+
+function distanceTravelledInFeet(valueBlock) {
+    return distanceFromHqInFeet(valueBlock) * 2112;
+}
